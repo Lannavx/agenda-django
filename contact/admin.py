72bc3e5795e3530d08c8ec9ba9f02384e35f9d4d
Lannavx <lanna_148@hotmail.com>
2024-10-24 14:44:34 -0300

    Criando os models e customizando as opções admin

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from contact import models
 
-# Register your models here.
+@admin.register(models.Contact)
+class ContactAdmin(admin.ModelAdmin):
+  '''Configura a interface de administração, 
+  definindo a exibição, ordenação, busca e paginação dos contatos'''
+  list_display = 'id', 'first_name', 'last_name', 'phone',
+  ordering = '-id',
+  search_fields = 'id', 'first_name', 'last_name',
+  list_per_page = 10
+  list_max_show_all = 100
+  list_display_links = 'id', 'phone',
+
+@admin.register(models.Category)
+class CategoryAdmin(admin.ModelAdmin):
+  '''Configura a interface de administração para o modelo Category, 
+  permitindo ordenar e visualizar as categorias'''
+  list_display = 'name',
+  ordering = '-id',
--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Category(models.Model):
+  '''Define o modelo para armazenar categorias 
+  que podem ser associadas a contatos'''
+  class Meta:
+    '''Define o nome singular e plural do modelo'''
+    verbose_name = 'Category'
+    verbose_name_plural = 'Categories'
+
+  name = models.CharField(max_length=50)
+
+  def __str__(self) -> str:
+    '''Representa a categoria através de seu nome'''
+    return self.name
+
+class Contact(models.Model):
+  '''Define o modelo para armazenar informações sobre contatos'''
+  first_name = models.CharField(max_length=50)
+  last_name = models.CharField(max_length=50, blank=True)
+  phone = models.CharField(max_length=50)
+  email = models.EmailField(max_length=250, blank=True)
+  created_date = models.DateTimeField(default=timezone.now)
+  description = models.TextField(blank=True)
+  show = models.BooleanField(default=True)
+  picture = models.ImageField(blank=True, upload_to='pictures/%Y/%m')
+  category = models.ForeignKey(
+    Category, 
+    on_delete=models.SET_NULL, 
+    blank=True, null=True
+  )
+  owner = models.ForeignKey(
+    User, 
+    on_delete=models.SET_NULL, 
+    blank=True, null=True
+  )
+
+
+  def __str__(self) -> str:
+    '''Representação textual do contato'''
+    return f'{self.first_name} {self.last_name}'
