0ea6c8b85bb200d82750df61fe9b8e20fbecb481
Lannavx <lanna_148@hotmail.com>
2024-11-01 10:17:27 -0300

    Atualizando as querysets do admin e contact_views

--- contact/admin.py
+++ contact/admin.py
@@ -4,12 +4,13 @@ from contact import models
 @admin.register(models.Contact)
 class ContactAdmin(admin.ModelAdmin):
   '''Configura a interface de administração, 
-  definindo a exibição, ordenação, busca e paginação dos contatos'''
-  list_display = 'id', 'first_name', 'last_name', 'phone',
+  definindo a exibição, ordenação, busca, edição e paginação dos contatos'''
+  list_display = 'id', 'first_name', 'last_name', 'phone', 'show',
   ordering = '-id',
   search_fields = 'id', 'first_name', 'last_name',
   list_per_page = 10
   list_max_show_all = 100
+  list_editable = 'first_name', 'last_name', 'show',
   list_display_links = 'id', 'phone',
 
 @admin.register(models.Category)
