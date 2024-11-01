e0788c0ecbd64d647d073694bbedd9b45241de98
Lannavx <lanna_148@hotmail.com>
2024-11-01 13:09:20 -0300

    Implementando e configurando o header do site

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, render, redirect
+from django.db.models import Q
 from contact.models import Contact
 
 def index(request):
@@ -6,6 +7,7 @@ def index(request):
 
   context = {
     'contacts': contacts,
+    'site_title': 'Contatos - '
   }
 
   return render(
@@ -14,11 +16,43 @@ def index(request):
     context
   )
 
+
+def search(request):
+  search_value = request.GET.get('q', '').strip()
+
+  if search_value == '':
+    return redirect('contact:index')
+  
+  contacts = Contact.objects \
+      .filter(show=True) \
+      .filter(
+        Q(first_name__icontains=search_value) |
+        Q(last_name__icontains=search_value) |
+        Q(phone__icontains=search_value) |
+        Q(email__icontains=search_value) 
+      ) \
+      .order_by('-id')
+
+  context = {
+    'contacts': contacts,
+    'site_title': 'Search - ',
+    'search_value': search_value,
+  }
+
+  return render(
+    request,
+    'contact/index.html', 
+    context
+  )
+
+
 def contact(request, contact_id):
   single_contact = get_object_or_404(Contact.objects, pk=contact_id, show=True)
+  site_title = f'{single_contact.first_name} {single_contact.last_name} - '
 
   context = {
     'contact': single_contact,
+    'site_title': site_title
   }
 
   return render(
